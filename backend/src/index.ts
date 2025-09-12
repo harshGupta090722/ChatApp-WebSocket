65893d5945d4df5cf2a5473007ac2d7c25137c1b
HarshGupta <harshgupta0907@gmail.com>
2025-09-13 03:58:00 +0530

    Created Basic Backend

---
+++ backend/src/index.ts
@@ -0,0 +1,39 @@
+import { WebSocketServer, WebSocket } from "ws";
+
+const wss = new WebSocketServer({ port: 8080 });
+
+interface User {
+    socket: WebSocket;
+    room: String;
+}
+
+let allSockets: User[] = [];
+
+wss.on("connection", (socket) => {
+
+    socket.on("message", (message) => {
+
+        const parsedMessage = JSON.parse(message as unknown as string);
+        if (parsedMessage.type == "join") {
+            console.log("User Joined Room " + parsedMessage.payload.roomId);
+            allSockets.push({
+                socket,
+                room: parsedMessage.payload.roomId
+            })
+        }
+        if (parsedMessage.type == "chat") {
+            console.log("User Wants to Chat");
+            let currentUserRoom = null;
+            for (let i = 0; i < allSockets.length; i++) {
+                if (allSockets[i]?.socket == socket) {
+                    currentUserRoom = allSockets[i]?.room
+                }
+            }
+            for (let i = 0; i < allSockets.length; i++) {
+                if (allSockets[i]?.room == currentUserRoom) {
+                    allSockets[i]?.socket.send(parsedMessage.payload.message)
+                }
+            }
+        }
+    })
+})
